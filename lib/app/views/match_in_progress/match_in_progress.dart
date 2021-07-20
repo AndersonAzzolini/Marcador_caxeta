c56a4ead422096bd7632a0c88eb632cddc38546f
Anderson Lucas Azzolini <lucas.azzolini@torfresma.com.br>
2021-07-20 11:28:00 -0300

    Alterado estilo da lista de partidas em andamento

--- lib/app/views/match_in_progress/match_in_progress.dart
+++ lib/app/views/match_in_progress/match_in_progress.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:untitled/app/controller/match/match_in_progress_controller.dart';
 import 'package:untitled/app/core/appColors.dart';
 
@@ -35,29 +36,81 @@ class MatchInProgress extends StatelessWidget {
                   itemBuilder: (context, key) {
                     InProgressMatch inProgressMatch =
                         List<InProgressMatch>.from(index.data)[key];
-                    return Card(
-                      child: ListTile(
-                        title: Text(inProgressMatch.name),
-                        subtitle: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(Icons.date_range),
-                                Text(inProgressMatch.dateCreate),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Icon(Icons.people),
-                                Text(inProgressMatch.amountPlayers)
-                              ],
-                            ),
-                          ],
+                    return TextButton(
+                      onPressed: () => {print(inProgressMatch.id)},
+                      child: SingleChildScrollView(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: AppColors.buttons,
+                              borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          )),
+                          height: 70,
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(inProgressMatch.name,
+                                  style: GoogleFonts.robotoSlab(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold)),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Text(
+                                        "Criado em:" +
+                                            inProgressMatch.dateCreate,
+                                        style: GoogleFonts.robotoSlab(
+                                            color: Colors.white,
+                                            fontSize: 13,
+                                            fontStyle: FontStyle.italic),
+                                      ),
+                                      Text(
+                                        "Participantes: " +
+                                            inProgressMatch.amountPlayers,
+                                        style: GoogleFonts.robotoSlab(
+                                            color: Colors.white,
+                                            fontSize: 13,
+                                            fontStyle: FontStyle.italic),
+                                      )
+                                    ],
+                                  )
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     );
+                    // return Card(
+                    //   child: ListTile(
+                    //     title: Text(inProgressMatch.name),
+                    //     subtitle: Column(
+                    //       mainAxisAlignment: MainAxisAlignment.start,
+                    //       crossAxisAlignment: CrossAxisAlignment.start,
+                    //       children: [
+                    //         Row(
+                    //           children: [
+                    //             Icon(Icons.date_range),
+                    //             Text(inProgressMatch.dateCreate),
+                    //           ],
+                    //         ),
+                    //         Row(
+                    //           children: [
+                    //             Icon(Icons.people),
+                    //             Text(inProgressMatch.amountPlayers)
+                    //           ],
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   ),
+                    // );
                   },
                 );
               }
